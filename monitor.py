ca2f5ba6ca5d007bc4d614097f28181562c71252
chindal <chindal80@naver.com>
2026-04-14 16:44:10 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,14 +1,13 @@
 import requests
 
-TOKEN = "8757989630:AAHybEkn0_QoH2YqEsLmVg1HjBFWlsu9vIs
-"
-CHAT_ID = "8770376534
-"
+TOKEN = "8757989630:AAHybEkn0_QoH2YqEsLmVg1HjBFWlsu9vIs"
+CHAT_ID = "8770376534"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
 res = requests.post(url, data={
     "chat_id": CHAT_ID,
-    "text": "🔥 직접 테스트"
+    "text": "🔥 테스트 성공"
 })
 
 print(res.text)
